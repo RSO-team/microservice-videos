2dcd44a3a0d6d753822874f02b227f5c934e9621
tstrus <tstrusplume.com>
2021-11-06 20:33:04 +0100

    first commit

---
+++ basketball-videos-api/src/main/java/si/fri/rsoteam/MicroserviceVideos.java
@@ -0,0 +1,13 @@
+package si.fri.rsoteam;
+
+/**
+ * Hello world!
+ *
+ */
+public class MicroserviceVideos
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ basketball-videos-api/src/test/java/si/fri/rsoteam/MicroserviceVideosTest.java
@@ -0,0 +1,20 @@
+package si.fri.rsoteam;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MicroserviceVideosTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
