7bd69241dbb05ab46f7354553d6454051f066c76
Luka Galjot <luka.galjot@gmail.com>
2021-12-04 23:29:38 +0100

    Fixes

--- basketball-videos-services/src/main/java/si/fri/rsoteam/services/beans/TagsBean.java
+++ basketball-videos-services/src/main/java/si/fri/rsoteam/services/beans/TagsBean.java
@@ -5,6 +5,7 @@ import si.fri.rsoteam.entities.TagEntity;
 import si.fri.rsoteam.lib.dtos.TagDto;
 import si.fri.rsoteam.services.mappers.TagMapper;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.Optional;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
+@RequestScoped
 public class TagsBean {
     private Logger log = Logger.getLogger(TagsBean.class.getName());
 
