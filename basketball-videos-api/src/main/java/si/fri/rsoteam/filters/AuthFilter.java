b89967715e9b1a00b5f73b52e35848e8f778682b
tstrus <tstrusplume.com>
2021-12-04 22:54:07 +0100

    yeet

--- basketball-videos-api/src/main/java/si/fri/rsoteam/filters/AuthFilter.java
+++ basketball-videos-api/src/main/java/si/fri/rsoteam/filters/AuthFilter.java
@@ -1,1 +1,44 @@
 package si.fri.rsoteam.filters;
+
+import si.fri.rsoteam.services.config.RestConfig;
+
+import javax.inject.Inject;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@WebFilter("*")
+public class AuthFilter implements Filter {
+    private final Logger LOG = Logger.getLogger(AuthFilter.class.getName());
+
+    @Inject
+    private RestConfig restConfig;
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
+            throws IOException, ServletException {
+        String apiToken = restConfig.getApiToken();
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        String token = httpServletRequest.getHeader("apiToken");
+        if (apiToken.equals(token)) {
+            chain.doFilter(request, response);
+        } else {
+            httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
