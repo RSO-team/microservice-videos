c0e87c80e2a3589487f83627db91f88ee90d22b7
Luka Galjot <luka.galjot@gmail.com>
2022-01-03 16:49:22 +0100

    Add application openapi annotations, add service discovery and enable etcd

--- basketball-videos-api/src/main/java/si/fri/rsoteam/MicroserviceVideos.java
+++ basketball-videos-api/src/main/java/si/fri/rsoteam/MicroserviceVideos.java
@@ -2,11 +2,26 @@ package si.fri.rsoteam;
 
 
 import com.kumuluz.ee.discovery.annotations.RegisterService;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@RegisterService(value = "basketball-videos")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Videos",
+                version = "v1.0.0",
+                contact = @Contact(),
+                license = @License(name = "none")
+        ),
+        servers = @Server(url = "http://localhost:8081"),
+        security = @SecurityRequirement(name = "none"))
+@RegisterService
 @ApplicationPath("/v1")
 public class MicroserviceVideos extends Application {
 
