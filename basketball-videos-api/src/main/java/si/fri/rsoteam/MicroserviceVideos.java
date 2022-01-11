f0c505a0b1e6d9bb5195ba03d0a4251f3712d046
tstrus <tstrusplume.com>
2022-01-11 17:15:22 +0100

    initial delay more

--- basketball-videos-api/src/main/java/si/fri/rsoteam/MicroserviceVideos.java
+++ basketball-videos-api/src/main/java/si/fri/rsoteam/MicroserviceVideos.java
@@ -19,7 +19,7 @@ import javax.ws.rs.core.Application;
                 contact = @Contact(),
                 license = @License(name = "none")
         ),
-        servers = @Server(url = "http://localhost:8081"),
+        servers = @Server(url = "http://52.226.192.49/videos"),
         security = @SecurityRequirement(name = "none"))
 @RegisterService
 @ApplicationPath("/v1")
